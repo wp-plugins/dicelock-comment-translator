8330b670fac282950efa5f2a8fafa5dc8b369af1
Angel Ferre <Angel Ferre@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-05 17:52:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/dicelock-comment-translator/trunk@132162 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dicelock-comment-translator.php
@@ -0,0 +1,134 @@
+<?php
+/*
+Plugin Name: DiceLock Comment Translator
+Plugin URI: http://blog.dicelocksecurity.com/plugins/dicelock-comment-translator/
+Description: Adds a link that shows a Google translation form to easily allow language translations while reading and/or before commenting. Visitors talking different languages can communicate with you in your language.
+Version: 1.0.0
+Author: Angel Ferre
+Author URI: hhttp://www.dicelocksecurity.com/
+*/
+
+class DiceLockCommentTranslator {
+	var $is_shown = false;
+	var $folder_url = false;
+	
+	function showCommentTranslator() {
+		global $smilies_themer;
+		
+		// Check if the translator form was already shown via direct calling
+		if ( $this->is_shown ) return;
+		
+		echo '<script language="javascript">';
+		echo '  function toggleDiceLockTranslator(divid){';
+		echo '    if(document.getElementById(divid).style.display == \'none\'){';
+		echo '      document.getElementById(divid).style.display = \'block\';';
+		echo '    }else{';
+		echo '      document.getElementById(divid).style.display = \'none\';';
+		echo '    }';
+		echo '  }';
+		echo '</script>';
+		
+		echo '<div align="center">';
+		echo '<a href="javascript:;" onmousedown="toggleDiceLockTranslator(\'DiceLockTranslator\');">Translation form - Translate your comment!</a>';
+		echo '</div>';
+		echo '<div id="DiceLockTranslator" style="display:none">';
+		//build the html form that references the google jsapi and the js
+		echo '<p>';
+		echo '<table class="googletranslate">';
+		echo '<thead>';
+		echo '<tr>';
+		echo '<th class="title">Precomment Google translator</th>';
+		echo '</tr>';
+		echo '</thead>';
+		echo '</table>';
+		echo 'If you are posting a comment in a language different than yours, you can use this translator to translate from your language to any language you want. As well you can copy any text of the page to see the translation.<br /><br />';
+		echo '</p>';
+		echo '<form class="DiceLockTranslator" onsubmit="return submitDiceLockTranslationChange();">';
+  		echo 'Insert text to translate:';
+  		echo '<textarea id="diceLockSource" style="width:98%;" rows="5">Enter Text to Translate Here</textarea><br /><br />';
+  		echo 'From: ';
+  		echo '<select name="diceLockSrc" id="diceLockSrc"></select> '; 
+  		echo 'To: ';
+  		echo '<select name="diceLockDst" id="diceLockDst"></select> ';
+  		echo '<input type="submit" value="Translate" class="DiceLockSubmit"/></form><br /><br />';
+  		echo 'Translation:';  
+  		echo '<div id="resultsDiceLockBody"></div>';
+  		echo '<div id="branding"></div>';
+		echo '</div>';
+
+		$this->print_translation_js();
+
+		echo '<br />';
+		$this->is_shown = true;
+	}
+	
+	function print_translation_js() {
+
+	?>
+		<script  language="javascript">
+		google.load("language", "1");
+		google.setOnLoadCallback(init);
+
+		function init() {
+   			var src = document.getElementById('diceLockSrc');
+   			var dst = document.getElementById('diceLockDst');
+   			var i = 0;
+   			for (l in google.language.Languages) {
+      				var lng = l.toLowerCase();
+      				var lngCode = google.language.Languages[l];
+      				if (google.language.isTranslatable(lngCode)) {
+         				src.options.add(new Option(lng, lngCode));
+         				dst.options.add(new Option(lng, lngCode));
+         			}
+      			}
+   			google.language.getBranding('branding', { type : 'vertical' });
+      			   
+   			submitDiceLockTranslationChange();
+		}
+   
+		function submitDiceLockTranslationChange() {
+   			var value = document.getElementById('diceLockSource').value;
+   			var src = document.getElementById('diceLockSrc').value;
+   			var dest = document.getElementById('diceLockDst').value;
+   			google.language.translate(value, src, dest, translateDiceLockTranslationResult);
+   			return false;
+		}
+   
+		function translateDiceLockTranslationResult(result) {
+   			var resultBody = document.getElementById("resultsDiceLockBody");
+   			if (result.translation) {
+      				var str = result.translation.replace('>', '&gt;').replace('<', '&lt;');
+      				resultBody.innerHTML = str;
+			}
+   			else {
+      				resultBody.innerHTML = '<span style="color:red">Error Translating</span>';
+      			}
+		}
+		</script>
+<?php	}
+	
+	function add_google_js () {
+		echo '<script src="http://www.google.com/jsapi?key=internal" type="text/javascript"></script>';
+	}
+
+}
+
+function dicelock_translator_show() {
+	global $dicelock_translator;
+	if ( $dicelock_translator && method_exists( $dicelock_translator, 'showCommentTranslator' ) )
+		$dicelock_translator->showCommentTranslator();
+}
+
+$dicelock_translator = new DiceLockCommentTranslator();
+$dicelock_translator->folder_url = WP_CONTENT_URL . '/plugins/' . plugin_basename( dirname(__FILE__) ) . "/";
+
+// If it was already shown through a theme call, this is overridden.
+add_action( 'get_footer', array( &$dicelock_translator, 'showCommentTranslator' ) );
+
+// Loads Google translator script
+add_action('wp_head', array( &$dicelock_translator, 'add_google_js' ) );
+
+register_activation_hook( __FILE__, array( &$dicelock_translator, 'install' ) );
+register_deactivation_hook( __FILE__, array( &$dicelock_translator, 'uninstall' ) );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,74 @@
+=== Plugin Name ===
+Contributors: Angel Ferre
+Donate link: http://blog.dicelocksecurity.com/plugins/dicelock-comment-translator/
+Tags: google translations, automatic translator, multilanguage, translator
+Requires at least: 2.2.0 or higher
+Tested up to: 2.8
+Stable tag: 1.0.0
+
+DiceLock Comment Translator inserts a language translation form at he bottom of your posts, pages and categories. The forms makes use of Google translation services and allows the visitor to tanslate between more than 43 different languages
+
+== Description ==
+
+DiceLock Comment Translator inserts a translation form at he bottom of your posts, pages and categories. The forms makes use of Google translation services and allows the visitor to tanslate between more than 43 different languages (depending in Google Translator): albanian, arabic, bulgarian, catalan, chinese, chinese (simplified), chinese (traditional), croatian, czech, danish, dutch, english, estonian, filipino, finnish, french, galician, german, greek, hebrew, hindi, hungarian, indonesian, italian, japanese, korean, latvian, lithuanian, maltese, norwegian, persian, polish, portuguese, romanian, russian, serbian, slovak, slovenian, spanish, swedish, tagalog, thai, turkish, ukrainian, vietnamese
+
+Main features:
+
+* Inserts a link to DiceLock Comment Translator form at the bottom of your posts, pages and categories.
+* DiceLock Comment Translator is hiden until the visitors makes click on the link that shows the translation form, it can be hiden again making click on the same link.
+* One translation engine: Google Translation Engine.
+* Allows copy and paste of text in order to be able your visitors to translate any kind of text.
+* Visitors can perform language translations on the fly.
+* The translation process is performed on the page, it does not require the page to be reloaded.
+* The plugin can be loaded once on each page.
+* No database modifications: DiceLock Comment Translator is not intrusive. It doesn’t create or alter any table on your database.
+
+For the latest information and changelog visit the website
+
+<a href="http://blog.dicelocksecurity.com/plugins/dicelock-comment-translator/">http://blog.dicelocksecurity.com/plugins/dicelock-comment-translator/</a>
+
+Information about WordPress (and plugins), and Joomla (and components and modules) customizations can be found at the website
+
+<a href="http://blog.dicelocksecurity.com/">http://blog.dicelocksecurity.com/</a>
+
+This plugin has been promoted by <a href="http://www.dicelocksecurity.com/">DiceLock Security</a>
+
+== Installation ==
+
+1. Upload the folder “dicelock-comment-translator” to the “wp-content/plugins” directory.
+2. Activate the plugin through the ‘Plugins’ menu in WordPress.
+3. And you have it working!. There is no more installation operations.
+
+== Frequently Asked Questions ==
+
+= Is the translation service always available? =
+
+As DiceLock Comment Translator does depend on Google translation services, it depends on Google disponibility.
+
+== Screenshots ==
+
+DiceLock Comment Translator inserts a link at the bottom and above of the footer of you pages, posts ans categories. This link allows to show/hide the translation form.
+
+1. Translator-form.JPG
+
+Once the form is shown your visitors get the following, they can insert the text that they want to translate in text area and select the original language and the language they want it to be translated.
+
+2. Translator-link.JPG
+
+== Changelog ==
+
+= 1.0.0 =
+* Plugin released.
+
+== Other Notes ==
+
+DiceLock Comment Translator can be loaded through a direct call to the function ‘dicelock_translator_show’ as in:
+
+	if (function_exists('dicelock_translator_show'))
+    		dicelock_translator_show();
+	else
+		echo 'Fix DiceLock Comment Translator';
+
+If it is loaded in this way, only the first called DiceLock Comment Translator link will be shown. 
+
+`<?php code(); // goes in backticks ?>`
